09b77fd53736f2ac0ff196108f298f0b5a8e6a21
Luciana Luz <lulu.cluz@gmail.com>
2022-08-21 18:06:42 -0300

    renomeando arquivo para seguir padrao de respostas das atividades

--- scripts/objetos_e_funcoes_r.R
+++ scripts/objetos_e_funcoes_r.R
@@ -4,6 +4,7 @@ vetor_frutas <- c("maca", "banana","laranja")
 vetor_frutas
 
 #objeto complexo
+LAhomes <- read.csv("https://assets.datacamp.com/production/course_3623/datasets/LAhomes.csv")
 laHomes <- lm(price ~ bed, LAhomes)
 laHomes
 
