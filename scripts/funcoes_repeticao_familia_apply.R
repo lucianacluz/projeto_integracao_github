6816c4b49d47450ad00c3a67ca25158ba858e24c
Luciana Luz <lulu.cluz@gmail.com>
2022-08-22 11:38:13 -0300

    exercicio funcoes de repeticao e familia apply

---
+++ scripts/funcoes_repeticao_familia_apply.R
@@ -0,0 +1,7 @@
+# funções de repetição - família apply
+
+cars 
+
+sapply(cars[ , 1:2], hist) #plota o histograma
+
+sapply(cars[ , 1:2], median) #emite a mediana de cada variavel do banco de dados cars
