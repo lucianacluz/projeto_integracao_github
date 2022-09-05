ea04449070c05cebc0f516705f89a13fc206bf44
Luciana Luz <lulu.cluz@gmail.com>
2022-09-05 19:38:30 -0300

    exercicio qui-quadrado - variaveis categoricas

---
+++ scripts/quiquadrado-variaveis_categoricas .R
@@ -0,0 +1,26 @@
+## QUI-QUADRADO COM R ##
+# CARREGANDO OS PACOTES
+pacman::p_load(data.table, ggplot2)
+
+# CARREGANDO A BASE DE DADOS CAR EVALUATION #
+breast_cancer <- fread('https://raw.githubusercontent.com/hugoavmedeiros/cp_com_r/master/bases_tratadas/breast_cancer.csv', stringsAsFactors = T)
+
+# TABELA DE CONTINGÊNCIA #
+breast_cancer_table <- table(breast_cancer$idade, breast_cancer$tumor_tamanho)
+breast_cancer_table
+
+# GRÁFICOS DE DISPERSÃO PAREADOS DAS VARIÁVEIS #
+
+ggplot(breast_cancer) + aes(x = tumor_tamanho, fill = idade) + geom_bar(position = "fill")
+
+# TESTE QUI QUADRADO #
+breast_cancer_test <- chisq.test(breast_cancer_table)
+breast_cancer_test
+breast_cancer_test$observed
+breast_cancer_test$expected
+
+# CORRPLOT DAS VARIÁVEIS #
+corrplot(breast_cancer_test$residuals, is.cor = FALSE)
+
+
+
