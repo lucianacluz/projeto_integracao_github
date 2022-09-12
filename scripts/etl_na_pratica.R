44430ead5675dbf7f74715140609a372be37f971
Luciana Luz <lulu.cluz@gmail.com>
2022-09-12 12:40:23 -0300

    ajuste na leitura da coluna data usando tolower - exercicio extracao

--- scripts/etl_na_pratica.R
+++ scripts/etl_na_pratica.R
@@ -1,15 +1,17 @@
 pacman::p_load(dplyr)
 
-sinistrosRecife2019Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/3531bafe-d47d-415e-b154-a881081ac76c/download/acidentes-2019.csv', sep = ';', encoding = 'UTF-8')
-sinistrosRecife2019Raw
-
 # carrega a base de sinistros de transito do site da PCR
-
 sinistrosRecife2019Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/3531bafe-d47d-415e-b154-a881081ac76c/download/acidentes-2019.csv', sep = ';', encoding = 'UTF-8')
 
 sinistrosRecife2020Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/fc1c8460-0406-4fff-b51a-e79205d1f1ab/download/acidentes_2020-novo.csv', sep = ';', encoding = 'UTF-8')
 
 sinistrosRecife2021Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2caa8f41-ccd9-4ea5-906d-f66017d6e107/download/acidentes_2021-jan.csv', sep = ';', encoding = 'UTF-8')
+
+#garantindo que todas as colunas estao em minusculo
+names(sinistrosRecife2019Raw) <- tolower(names(sinistrosRecife2019Raw))
+names(sinistrosRecife2020Raw) <- tolower(names(sinistrosRecife2020Raw))
+names(sinistrosRecife2021Raw) <- tolower(names(sinistrosRecife2021Raw))
+
 
 # junta as bases de dados com comando rbind (juntas por linhas)
 colunas_iguais_parte_1 <- names(sinistrosRecife2020Raw[
@@ -55,7 +57,7 @@ naZero <- function(x) {
 }
 
 # aplica a função naZero a todas as colunas de contagem
-sinistrosRecifeRaw[, 13:23] <- sapply(sinistrosRecifeRaw[, 13:23], naZero)
+sinistrosRecifeRaw[, 14:24] <- sapply(sinistrosRecifeRaw[, 14:24], naZero)
 
 # exporta em formato nativo do R
 saveRDS(sinistrosRecifeRaw, "bases_tratadas/sinistrosRecife.rds")
